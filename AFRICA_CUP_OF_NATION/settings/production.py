4cc1d894de0db28d237edd3cecd518104aca7647
divanshu <divanshu79@github.com>
2018-09-17 21:54:18 +0530

    update

--- AFRICA_CUP_OF_NATION/settings/production.py
+++ AFRICA_CUP_OF_NATION/settings/production.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = 'bfd506am3t9(ousne#0&!z4q%7!9tj(3n_@w&#16caaz%2yy4m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['project-name.herokuapp.com']
 
